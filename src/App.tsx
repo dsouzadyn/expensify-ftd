5e5e4b73dacf37bdf6085a9427ade42941cb5370
Dylan Dsouza <dsouzadyn@gmail.com>
2021-05-22 03:25:14 +0530

    completed login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,29 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
+  Flex,
+  Spacer,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { LoginForm } from "./components/login-form.component"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+      <Grid>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
+        <Flex>
+          <Spacer />
+          <Box w="320px">
+            <LoginForm />
+          </Box>
+          <Spacer />
+        </Flex>
       </Grid>
+
+
     </Box>
   </ChakraProvider>
 )
---
+++ src/servies/auth.service.tsx
@@ -0,0 +1,14 @@
+import * as qs from "querystring";
+
+export const fetchToken = async (username: string, password: string) => {
+    const response = await fetch("http://127.0.0.1:8000/login", {
+        method: 'POST', headers: {
+            //'Content-Type': 'application/json'
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }, body: qs.stringify({
+            username: username,
+            password: password
+        })
+    });
+    return response.json();
+}
